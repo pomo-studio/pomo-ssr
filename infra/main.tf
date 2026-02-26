154186b45534a2b324d5e5598fdcf9186cfdaca9
Andre Pitanga <android.pitanga@gmail.com>
2026-02-25 22:57:00 -0500

    chore: switch SSR module source to registry v2.4.13

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,8 @@
 # When forking, replace values marked ← with your own.
 
 module "ssr" {
-  source = "git::https://github.com/pomo-studio/terraform-aws-serverless-ssr.git?ref=v2.4.12"
+  source  = "pomo-studio/serverless-ssr/aws"
+  version = "= 2.4.13"
 
   providers = {
     aws.primary = aws.primary
